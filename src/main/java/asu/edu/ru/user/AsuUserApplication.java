f1c906e6be292e28d2c3765c53215471eb0d1bf6
Dan Hramtsov <dan.hramtsov@gmail.com>
2021-11-30 14:02:24 +0400

    init

---
+++ src/main/java/asu/edu/ru/user/AsuUserApplication.java
@@ -0,0 +1,13 @@
+package asu.edu.ru.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsuUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsuUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asu/edu/ru/user/AsuUserApplicationTests.java
@@ -0,0 +1,13 @@
+package asu.edu.ru.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsuUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
